d46ab73ea2fc4c4c132088ca4c0a1d3f0edcfb98
PriyeshLenka <priyeshlenka50@gmail.com>
2024-03-10 14:41:34 +0530

    login pages changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import LoginPage from '../src/pages/LoginPage/LoginPage'
+import LandingPagev1 from './pages/LandingPage/LandingPagev1'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <LandingPagev1/>
     <LoginPage/>
-      
     </>
   )
 }
